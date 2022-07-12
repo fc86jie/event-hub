fb98142beee3f12ab2d4103941730525b8c958cc
fc86jie <wangrenjie86@gmail.com>
2022-07-12 18:48:51 +0800

    test: add unit test

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,12 @@
  * @Author: wangrenjie@zenmen.com
  * @Date: 2022-07-12 15:43:27
  * @LastEditors: wangrenjie@zenmen.com
- * @LastEditTime: 2022-07-12 18:25:01
+ * @LastEditTime: 2022-07-12 18:35:27
  * @FilePath: \event-hub\src\index.ts
  * @Description:
  */
 const calc = (a: number, b: number): number => {
   return a - b
 }
-console.log(calc(10, 3))
+
+export default calc
