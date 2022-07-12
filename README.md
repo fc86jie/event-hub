66961babbf0dceb956ff89e4303de91977859a04
fc86jie <wangrenjie86@gmail.com>
2022-07-12 18:22:59 +0800

    'ci:add'

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # event-hub
+
+### [参考链接](https://mp.weixin.qq.com/s/3_gZ9vNZixK_qZdWu9Dyhw)
---
+++ src/index.ts
@@ -0,0 +1,12 @@
+/*
+ * @Author: wangrenjie@zenmen.com
+ * @Date: 2022-07-12 15:43:27
+ * @LastEditors: wangrenjie@zenmen.com
+ * @LastEditTime: 2022-07-12 17:04:08
+ * @FilePath: \event-hub\src\index.ts
+ * @Description:
+ */
+const calc = (a: number, b: number): number => {
+  return a - b
+}
+console.log(calc(10, 2))
